21b01d97cdedacbf168773695083caf1941a69ad
Yoon-Mac <sryoowk@gmail.com>
2025-06-16 11:50:41 +0900

    codebase setting

---
+++ eBookViwerApp/eBookViwerApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  eBookViwerApp
+//
+//  Created by Yoon on 6/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureUI()
+    }
+
+}
+
+private func configureUI(){
+    print("configureUI")
+}
---
+++ eBookViwerApp/eBookViwerAppTests/eBookViwerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  eBookViwerAppTests.swift
+//  eBookViwerAppTests
+//
+//  Created by Yoon on 6/16/25.
+//
+
+import Testing
+@testable import eBookViwerApp
+
+struct eBookViwerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
